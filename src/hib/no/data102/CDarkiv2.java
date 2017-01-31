319c76f97d86f6c45acc92a86168922b6d013727
sig <h142552@stud.hib.no>
2017-01-31 12:09:04 +0100

    Lagt til CDarkiv2 og LinearNode klasser

---
+++ src/hib/no/data102/CDarkiv2.java
@@ -0,0 +1,49 @@
+package hib.no.data102;
+
+import hib.no.data102.adt.CDarkivADT;
+
+public class CDarkiv2 implements CDarkivADT {
+
+	@Override
+	public CD[] hentCdTabell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void leggTilCd(CD nyCd) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean slettCd(int cdNr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CD[] sokTittel(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CD[] sokArtist(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hentAntall() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int hentAntall(Sjanger sjanger) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
